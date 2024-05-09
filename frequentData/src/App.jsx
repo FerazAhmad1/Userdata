dbfde04c4fd81797d944aa35829961b06a482494
Feraz ahmad <ferazahmadgit62@gmail.com>
2024-05-09 19:00:52 +0530

    basic setup vite with react template with tailwind css

---
+++ frequentData/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-800 w-9 h-9 ">
+        <h1 className="text-white">Hellow</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
