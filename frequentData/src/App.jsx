5ecfc59fe9803d807b371c2412d412fad9491760
Feraz ahmad <ferazahmadgit62@gmail.com>
2024-05-10 15:13:58 +0530

    Radio button circle not visisble need to beautify

--- frequentData/src/App.jsx
+++ frequentData/src/App.jsx
@@ -1,9 +1,9 @@
+import Form from "./components/Form";
+
 function App() {
   return (
     <>
-      <div className="bg-red-800 w-9 h-9 ">
-        <h1 className="text-white">Hellow</h1>
-      </div>
+      <Form />
     </>
   );
 }
